2b92ad5dacda44a59ff981469209f0b8579bb759
Simon Dann <carbontwelve@users.noreply.github.com>
2017-11-22 21:56:40 +0000

    :white_check_mark: unit test now setups and drops database

--- tests/PluginTest.php
+++ tests/PluginTest.php
@@ -1,13 +1,56 @@
 <?php
 namespace TapestryCloud\Asset\Tests;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\SchemaTool;
+use Doctrine\ORM\Tools\Setup;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\NullOutput;
 use Tapestry\Console\DefaultInputDefinition;
+use Tapestry\Entities\Configuration;
 use Tapestry\Entities\Project;
 use Tapestry\Generator;
 use Tapestry\Tapestry;
+
 class PluginTest extends \PHPUnit_Framework_TestCase
 {
+
+    protected static $em;
+
+    /**
+     * This method is called before the first test of this test class is run.
+     *
+     * @inheritdoc
+     */
+    public static function setUpBeforeClass()
+    {
+        $configuration = new Configuration(include __DIR__ . '/mock_project/config.php');
+
+        $em = EntityManager::create(
+        $configuration->get('plugins.database', []),
+            Setup::createAnnotationMetadataConfiguration(
+                [
+                    realpath(__DIR__ . '/../src/Entities')
+                ],
+                true
+            )
+        );
+        $tool = new SchemaTool($em);
+        $tool->createSchema($em->getMetadataFactory()->getAllMetadata());
+
+        self::$em = $em;
+    }
+
+    /**
+     * This method is called after the last test of this test class is run.
+     *
+     * @inheritdoc
+     */
+    public static function tearDownAfterClass()
+    {
+        $tool = new SchemaTool(self::$em);
+        $tool->dropDatabase();
+    }
+
     public function testPlugin(){
         $definitions = new DefaultInputDefinition();
         $tapestry = new Tapestry(new ArrayInput([
