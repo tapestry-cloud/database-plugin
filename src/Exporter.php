49b9b555a9ddb8c73420e183374961f5fcc7bd34
Simon Dann <carbontwelve@users.noreply.github.com>
2017-11-23 01:27:08 +0000

    :sparkles: write to output

--- src/Exporter.php
+++ src/Exporter.php
@@ -5,10 +5,12 @@ namespace TapestryCloud\Database;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use PDO;
+use Symfony\Component\Console\Output\OutputInterface;
 use Tapestry\Entities\Collections\FlatCollection;
 use Tapestry\Entities\Project;
 use Doctrine\DBAL\Connection;
 use Tapestry\Modules\ContentTypes\ContentTypeFactory;
+use Tapestry\Tapestry;
 use TapestryCloud\Database\Entities\ContentType;
 use TapestryCloud\Database\Entities\Environment;
 use TapestryCloud\Database\Entities\File;
@@ -21,18 +23,27 @@ class Exporter {
      * @var EntityManagerInterface|EntityManager
      */
     private $entityManager;
+    /**
+     * @var OutputInterface
+     */
+    private $output;
 
     /**
      * Exporter constructor.
      * @param EntityManagerInterface $entityManager
-     * @throws \Exception
+     * @param Tapestry $tapestry
      */
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(EntityManagerInterface $entityManager, Tapestry $tapestry)
     {
         $this->entityManager = $entityManager;
+        $this->output = $tapestry->getContainer()->get(OutputInterface::class);
+
     }
 
     public function export(Project $project) {
+
+        $this->output->writeln('[$] Syncing with database.');
+
         /** @var FlatCollection $files */
         $files = $project->get('files');
 
